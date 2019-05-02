2e1e8984428f76a4adc08d5fd0ac0a4017949f31
jismisk <jismisabuk@utexas.edu>
2019-05-02 00:24:01 -0500

    adding cases for flt reg reg, control flow, atomic, 32 reg-reg

--- ariane/tb_new/scoreboard.sv
+++ ariane/tb_new/scoreboard.sv
@@ -273,6 +273,362 @@ function scoreboard_entry_t decoder_scoreboard::getresult_scoreboard_entry;
                     endcase
                 end
 
+//---------------------------------------------------------------------------
+//			32 BIT REG - REG				   //
+//---------------------------------------------------------------------------
+
+
+        	7'b0111011: begin
+        		collect_instr_o.fu  = (instr_test.rtype.funct7 == 7'd1) ? MULT : ALU;
+        	        collect_instr_o.rs1[4:0] = instr_test.rtype.rs1;
+        	        collect_instr_o.rs2[4:0] = instr_test.rtype.rs2;
+        	        collect_instr_o.rd[4:0]  = instr_test.rtype.rd;
+
+        	        case ({instr_test.rtype.funct7, instr_test.rtype.funct3})
+        	             {7'b0000000, 3'b000}: collect_instr_o.op = ADDW; // addw
+        	             {7'b0100000, 3'b000}: collect_instr_o.op = SUBW; // subw
+        	             {7'b0000000, 3'b001}: collect_instr_o.op = SLLW; // sllw
+        	             {7'b0000000, 3'b101}: collect_instr_o.op = SRLW; // srlw
+        	             {7'b0100000, 3'b101}: collect_instr_o.op = SRAW; // sraw
+        	             // Multiplications
+        	             {7'b0000001, 3'b000}: collect_instr_o.op = MULW;
+        	             {7'b0000001, 3'b100}: collect_instr_o.op = DIVW;
+        	             {7'b0000001, 3'b101}: collect_instr_o.op = DIVUW;
+        	             {7'b0000001, 3'b110}: collect_instr_o.op = REMW;
+        	             {7'b0000001, 3'b111}: collect_instr_o.op = REMUW;
+        	             default: illegal_instr = 1'b1;
+        	        endcase
+        	end
+
+
+////////////////////////////////////////////////////////////////////////////
+
+///////////		ATOMIC OPERATIONS			////////////
+
+////////////////////////////////////////////////////////////////////////////
+
+
+
+		7'b0101111: begin		//
+			collect_instr_o.fu = STORE;		
+			collect_instr_o.rs1[4:0] = instr_test.atype.rs1;
+			collect_instr_o.rs2[4:0] = instr_test.atype.rs2;
+			collect_instr_o.rd[4:0] = instr_test.atype.rd;
+
+			case (instr_test.stype.funct3)
+				3'h2: begin
+				    case (instr_test.instr[31:27])
+        	                    	5'h0:  collect_instr_o.op = AMO_ADDW;
+        	                    	5'h1:  collect_instr_o.op = AMO_SWAPW;
+        	                    	5'h2:  begin 
+					       collect_instr_o.op = AMO_LRW;
+        	                               if (instr_test.atype.rs2 != 0) illegal_instr = 1'b1;
+					end
+        	                    	5'h3:  collect_instr_o.op = AMO_SCW;
+        	                    	5'h4:  collect_instr_o.op = AMO_XORW;
+        	                    	5'h8:  collect_instr_o.op = AMO_ORW;
+        	                    	5'hC:  collect_instr_o.op = AMO_ANDW;
+        	                    	5'h10: collect_instr_o.op = AMO_MINW;
+        	                    	5'h14: collect_instr_o.op = AMO_MAXW;
+        	                    	5'h18: collect_instr_o.op = AMO_MINWU;
+        	                    	5'h1C: collect_instr_o.op = AMO_MAXWU;
+        	                    	default: illegal_instr = 1'b1;
+				    endcase
+				end
+				3'h3: begin
+				    case (instr_test.instr[31:27])
+        	                        5'h0:  collect_instr_o.op = AMO_ADDD;
+        	                        5'h1:  collect_instr_o.op = AMO_SWAPD;
+        	                        5'h2:  begin
+					       collect_instr_o.op = AMO_LRD;
+        	                               if (instr_test.atype.rs2 != 0) illegal_instr = 1'b1;
+					end
+        	                        5'h3:  collect_instr_o.op = AMO_SCD;
+        	                        5'h4:  collect_instr_o.op = AMO_XORD;
+        	                        5'h8:  collect_instr_o.op = AMO_ORD;
+        	                        5'hC:  collect_instr_o.op = AMO_ANDD;
+        	                        5'h10: collect_instr_o.op = AMO_MIND;
+        	                        5'h14: collect_instr_o.op = AMO_MAXD;
+        	                        5'h18: collect_instr_o.op = AMO_MINDU;
+        	                        5'h1C: collect_instr_o.op = AMO_MAXDU;
+        	                    	default: illegal_instr = 1'b1;
+        	                    endcase
+				 end
+        	                 default: illegal_instr = 1'b1;			
+			endcase
+		    
+
+		end
+		
+////////////////////////////////////////////////////////////////////////////
+
+///////////			CONTROL FLOW			////////////
+
+////////////////////////////////////////////////////////////////////////////
+
+
+		7'b1100011: begin		//Compare
+			collect_instr_o.fu = CTRL_FLOW;		//Control flow
+			collect_instr_o.rs1[4:0] = instr_test.stype.rs1;
+			collect_instr_o.rs2[4:0] = instr_test.stype.rs2;
+	//		control_flow = 1'd1;
+        	        collect_instr_o.result = sb_imm(tx_in.instruction_i);
+        	        collect_instr_o.use_imm = 1'b1;
+
+			case(instr_test.stype.funct3)
+				3'b000:collect_instr_o.op = EQ;
+				3'b001:collect_instr_o.op = NE;
+				3'b100:collect_instr_o.op = LTS;
+				3'b110:collect_instr_o.op = LTU;
+				3'b001:collect_instr_o.op = GES; //Wrong ??? bug????
+				3'b111:collect_instr_o.op = GEU;
+ 				default: begin
+        //	                    control_flow = 1'b0;
+        	                    illegal_instr = 1'b1;
+        	                end
+				
+			endcase
+		end
+
+		7'b1100111: begin		//JALR
+			collect_instr_o.fu = CTRL_FLOW;
+			collect_instr_o.rs1[4:0] 	= instr_test.itype.rs1;
+			collect_instr_o.rd[4:0]   	= instr_test.itype.rd;
+	//		control_flow 	= 1'd1;
+        	        collect_instr_o.result 	= i_imm(tx_in.instruction_i);
+        	        collect_instr_o.use_imm 	= 1'b1;
+			case(instr_test.stype.funct3)
+				3'b000:collect_instr_o.op = JALR;
+				default: begin
+        	          //          control_flow = 1'b0;  //Bug????
+        	                    illegal_instr = 1'b1;
+        	                end
+
+			endcase
+		end
+
+		7'b1101111: begin		//JAL
+			collect_instr_o.fu = CTRL_FLOW;
+			collect_instr_o.rd[4:0]   	= instr_test.itype.rd;
+	//		control_flow 	= 1'd1;
+        	        collect_instr_o.result 	= uj_imm(tx_in.instruction_i);
+        	        collect_instr_o.use_imm 	= 1'b1;
+		end
+
+		7'b0010111: begin		//AUIPC
+			collect_instr_o.fu = ALU;
+			collect_instr_o.rd[4:0]   	= instr_test.itype.rd;
+        	        collect_instr_o.result 	= { {32 {tx_in.instruction_i[31]}}, tx_in.instruction_i[31:12], 12'b0 };
+        	        collect_instr_o.use_imm 	= 1'b1;
+			collect_instr_o.use_pc 	= 1'b1;
+			
+		end
+//illegal operand case !!!!
+		7'b0010111: begin		//LUI
+			collect_instr_o.fu = ALU;
+			collect_instr_o.rd[4:0]   	= instr_test.itype.rd;
+        	        collect_instr_o.result 	= { {32 {tx_in.instruction_i[31]}}, tx_in.instruction_i[31:12], 12'b0 };
+        	        collect_instr_o.use_imm 	= 1'b1;
+		end
+
+// ----------------------------------
+// Floating-Point Reg-Reg Operations
+// ----------------------------------
+		7'b1000011, 7'b1000111, 7'b1001011, 7'b1001111 : begin
+        		if (tx_in.fs_i != riscv::Off) begin 
+        	                collect_instr_o.fu  = FPU;
+        	                collect_instr_o.rs1 = instr_test.r4type.rs1;
+        	                collect_instr_o.rs2 = instr_test.r4type.rs2;
+        	                collect_instr_o.rd  = instr_test.r4type.rd;
+        	          	collect_instr_o.result = {59'b0, instr_test.r4type.rs3};
+        	        	collect_instr_o.use_imm = 1'b0;
+        	                case (instr_test.r4type.opcode)
+        	                    default:      collect_instr_o.op = FMADD;  
+        	                    7'b1000111:   collect_instr_o.op = FMSUB;  
+        	                    7'b1001011:   collect_instr_o.op = FNMSUB; 
+        	                    7'b1001111:   collect_instr_o.op = FNMADD;
+        	                endcase
+
+        	                case (instr_test.r4type.funct2)
+        	                    2'b00: if (~RVF)             illegal_instr = 1'b1;
+        	                    2'b01: if (~RVD)             illegal_instr = 1'b1;
+        	                    2'b10: if (~XF16 & ~XF16ALT) illegal_instr = 1'b1;
+        	                    2'b11: if (~XF8)             illegal_instr = 1'b1;
+        	                    default: illegal_instr = 1'b1;
+        	                endcase
+
+
+        	                case (instr_test.rftype.rm) inside
+        	                    [3'b000:3'b100]: ; 
+        	                    3'b101: begin      //extra rounding mode specific to ariane
+        	                        if (~XF16ALT || instr_test.rftype.fmt != 2'b10)
+        	                            illegal_instr = 1'b1;
+        	                        case (tx_in.frm_i) inside 
+        	                            [3'b000:3'b100]: ; 
+        	                            default : illegal_instr = 1'b1;
+        	                        endcase
+        	                    end
+        	                    3'b111: begin	//extra rounding mode specific to ariane
+        	                        case (tx_in.frm_i) inside
+        	                            [3'b000:3'b100]: ; 
+        	                            default : illegal_instr = 1'b1;
+        	                        endcase
+        	                    end
+        	                    default : illegal_instr = 1'b1;
+        	                endcase
+			end else begin
+				illegal_instr = 1'b1;
+        	        end
+		end
+
+		7'b1010011 : begin
+        		if (tx_in.fs_i != riscv::Off) begin 
+        	                collect_instr_o.fu  = FPU;
+        	                collect_instr_o.rs1 = instr_test.rftype.rs1;
+        	                collect_instr_o.rs2 = instr_test.rftype.rs2;
+        	                collect_instr_o.rd  = instr_test.rftype.rd;
+        	                check_fprm        = 1'b1;
+        	                case (instr_test.rftype.funct5)
+        	                    5'b00000: begin
+        	                        collect_instr_o.op  = FADD;           
+        	                        collect_instr_o.rs1 = '0;     //Wrong??         
+        	                        collect_instr_o.rs2 = instr_test.rftype.rs1;
+        	        		collect_instr_o.result = i_imm(tx_in.instruction_i);
+        	        		collect_instr_o.use_imm = 1'b1;
+					           
+        	                    end
+        	                    5'b00001: begin
+        	                        collect_instr_o.op  = FSUB;  
+        	                        collect_instr_o.rs1 = '0;    //Wrong??
+        	                        collect_instr_o.rs2 = instr_test.rftype.rs1; 
+        	               		collect_instr_o.result = i_imm(tx_in.instruction_i);
+        	        		collect_instr_o.use_imm = 1'b1;
+        	                    end
+        	                    5'b00010: collect_instr_o.op = FMUL;  
+        	                    5'b00011: collect_instr_o.op = FDIV; 
+        	                    5'b01011: begin
+        	                        collect_instr_o.op = FSQRT; 
+        	                        if (instr_test.rftype.rs2 != 5'b00000) illegal_instr = 1'b1;
+        	                    end
+        	                    5'b00100: begin
+        	                        collect_instr_o.op = FSGNJ; 
+        	                        check_fprm       = 1'b0;  
+        	                        if (XF16ALT) begin        
+        	                            if (!(instr_test.rftype.rm inside {[3'b000:3'b010], [3'b100:3'b110]})) //Wrong ?? extra rounding modes
+        	                                illegal_instr = 1'b1;
+        	                        end else begin
+        	                            if (!(instr_test.rftype.rm inside {[3'b000:3'b010]}))
+        	                                illegal_instr = 1'b1;
+        	                        end
+        	                    end
+        	                    5'b00101: begin
+        	                        collect_instr_o.op = FMIN_MAX; 
+        	                        check_fprm       = 1'b0;     
+        	                        if (XF16ALT) begin           
+        	                            if (!(instr_test.rftype.rm inside {[3'b000:3'b001], [3'b100:3'b101]})) //Wrong?? extra rounding modes
+        	                                illegal_instr = 1'b1;
+        	                        end else begin
+        	                            if (!(instr_test.rftype.rm inside {[3'b000:3'b001]}))
+        	                                illegal_instr = 1'b1;
+        	                        end
+        	                    end
+        	                    5'b01000: begin
+        	                        collect_instr_o.op  = FCVT_F2F; 
+        	                        collect_instr_o.rs2 = instr_test.rvftype.rs1; 				//Wrong??
+        	                	collect_instr_o.result = i_imm(tx_in.instruction_i);
+        	        		collect_instr_o.use_imm = 1'b1;
+        	                        if (instr_test.rftype.rs2[24:23]) illegal_instr = 1'b1; 
+        	                        case (instr_test.rftype.rs2[22:20])
+        	                            3'b000: if (~RVF)     illegal_instr = 1'b1;
+        	                            3'b001: if (~RVD)     illegal_instr = 1'b1;
+        	                            3'b010: if (~XF16)    illegal_instr = 1'b1;
+        	                            3'b110: if (~XF16ALT) illegal_instr = 1'b1;
+        	                            3'b011: if (~XF8)     illegal_instr = 1'b1;
+        	                            default: illegal_instr = 1'b1;
+        	                        endcase
+        	                    end
+        	                    5'b10100: begin
+        	                        collect_instr_o.op = FCMP; 
+        	                        check_fprm       = 1'b0; 
+        	                        if (XF16ALT) begin      
+        	                            if (!(instr_test.rftype.rm inside {[3'b000:3'b010], [3'b100:3'b110]})) // Wrong?? Extra rounding modes
+        	                                illegal_instr = 1'b1;
+        	                        end else begin
+        	                            if (!(instr_test.rftype.rm inside {[3'b000:3'b010]}))
+        	                                illegal_instr = 1'b1;
+        	                        end
+        	                    end
+        	                    5'b11000: begin							//Wrong?? wht are these
+        	                        collect_instr_o.op = FCVT_F2I; 
+        	                    	collect_instr_o.result = i_imm(tx_in.instruction_i);
+        	        		collect_instr_o.use_imm = 1'b1;
+        	                        if (instr_test.rftype.rs2[24:22]) illegal_instr = 1'b1; 
+        	                    end
+        	                    5'b11010: begin							//Wrong?? wht are these
+        	                        collect_instr_o.op = FCVT_I2F;  
+        	                  	collect_instr_o.result = i_imm(tx_in.instruction_i);
+        	        		collect_instr_o.use_imm = 1'b1;
+        	                        if (instr_test.rftype.rs2[24:22]) illegal_instr = 1'b1; 
+        	                    end
+        	                    5'b11100: begin
+        	                        collect_instr_o.rs2 = instr_test.rftype.rs1; 
+        	                        check_fprm        = 1'b0; 
+        	                        if (instr_test.rftype.rm == 3'b000 || (XF16ALT && instr_test.rftype.rm == 3'b100)) 
+        	                            collect_instr_o.op = FMV_F2X;       
+        	                        else if (instr_test.rftype.rm == 3'b001 || (XF16ALT && instr_test.rftype.rm == 3'b101)) 
+        	                            collect_instr_o.op = FCLASS; 
+        	                        else illegal_instr = 1'b1;
+        	                        
+        	                        if (instr_test.rftype.rs2 != 5'b00000) illegal_instr = 1'b1;
+        	                    end
+        	                    5'b11110: begin							//Wrong??
+        	                        collect_instr_o.op = FMV_X2F; 
+        	                        collect_instr_o.rs2 = instr_test.rftype.rs1; 
+        	                        check_fprm       = 1'b0;
+        	                        if (!(instr_test.rftype.rm == 3'b000 || (XF16ALT && instr_test.rftype.rm == 3'b100)))
+        	                            illegal_instr = 1'b1;
+        	                        if (instr_test.rftype.rs2 != 5'b00000) illegal_instr = 1'b1;
+        	                    end
+        	                    default : illegal_instr = 1'b1;
+        	                endcase
+
+        	                // check format
+        	                case (instr_test.rftype.fmt)
+        	                    2'b00: if (~RVF)             illegal_instr = 1'b1;
+        	                    2'b01: if (~RVD)             illegal_instr = 1'b1;
+        	                    2'b10: if (~XF16 & ~XF16ALT) illegal_instr = 1'b1;
+        	                    2'b11: if (~XF8)             illegal_instr = 1'b1;
+        	                    default: illegal_instr = 1'b1;
+        	                endcase
+
+        	                // check rounding mode
+        	                if (check_fprm) begin
+        	                    case (instr_test.rftype.rm) inside
+        	                        [3'b000:3'b100]: ; 
+        	                        3'b101: begin      
+        	                            if (~XF16ALT || instr_test.rftype.fmt != 2'b10)
+        	                                illegal_instr = 1'b1;
+        	                            case (tx_in.frm_i) inside 
+        	                                [3'b000:3'b100]: ;
+        	                                default : illegal_instr = 1'b1;
+        	                            endcase
+        	                        end
+        	                        3'b111: begin
+        	                            case (tx_in.frm_i) inside
+        	                                [3'b000:3'b100]: ; 
+        	                                default : illegal_instr = 1'b1;
+        	                            endcase
+        	                        end
+        	                        default : illegal_instr = 1'b1;
+        	                    endcase
+        	                end
+        	            end else begin
+        	                illegal_instr = 1'b1;
+        	            end
+		end
+
+
+
 	endcase
       end
 
