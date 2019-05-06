4626671d00614ac35f325fb443c1ba7617981bce
Shvetha Senthil Kumar <ssenthilkumar@wario.(none)>
2019-05-06 00:24:11 -0500

    constraints added for integer,imm,v_float instructions

--- ariane/tb_new/sequences.sv
+++ ariane/tb_new/sequences.sv
@@ -41,7 +41,15 @@ import ariane_pkg::*;
 	constraint restrict_32_regreg{(opcode==7'b0111011);}
 	constraint restrict_fp_32_regreg_legal{(opcode==7'b1000011)||(opcode==7'b1000111)|| (opcode==7'b1001011)||(opcode== 7'b1001111)||(opcode== 7'b1010011) ;}
 	constraint restrict_control_legal{(opcode==7'b1100011) || (opcode==7'b1100111) || (opcode==7'b1101111) || (opcode==7'b0010111) || (opcode==7'b0010111);}
- 
+
+	//shvetha constraints
+	constraint restrict_integer_reg{(opcode==7'b0110011)->((funct7==7'b0000000)||(funct7==7'b0100000)||(funct7==7'b0000001));}
+	
+	constraint restrict_reg_imm_funct3{(opcode==7'b0011011)->((funct3==3'b000)||(funct3==3'b001)||(funct3==101));}
+	constraint restrict_reg_imm_funct7_sll{((opcode==7'b0011011)&&(funct3==3'b001))->(funct7==7'b0000000);}
+	constraint restrict_reg_imm_funt7_srl_sra{((opcode==7'b0011011)&&(funct3==3'b101))->((funct7==7'b0000000)||(funct7==7'b0100000));}
+	
+	constraint restrict_vec_float{(opcode==7'b0110011)->(instruction_i[31:30] == 2'b10);} 
 
         function new(string name = "");
             super.new(name);
@@ -52,7 +60,7 @@ import ariane_pkg::*;
         endfunction: convert2string
 	
 	function void post_randomize();
-		if(opcode == 7'b0100011) begin
+		if((opcode == 7'b0100011)||(opcode == 7'b0110011)||(opcode == 7'b0011011)) begin
 			instruction_i[31:0] = {imm[11:5],rs2[4:0],rs1[4:0],funct3,imm[4:0],opcode};
 		end
 		else if(opcode == 7'b0000011) begin
@@ -101,7 +109,12 @@ import ariane_pkg::*;
 	    tx.restrict_32_regreg.constraint_mode(0);
 	    tx.restrict_fp_32_regreg_legal.constraint_mode(0);
 	    tx.restrict_control_legal.constraint_mode(0);
-	    
+	    tx.restrict_integer_reg.constraint_mode(0);
+	    tx.restrict_reg_imm_funct3.constraint_mode(0);
+	    tx.restrict_reg_imm_funct7_sll.constraint_mode(0);
+	    tx.restrict_reg_imm_funt7_srl_sra.constraint_mode(0);
+	    tx.restrict_vec_float.constraint_mode(0);
+
             start_item(tx);
             assert(tx.randomize());
             finish_item(tx);
@@ -126,6 +139,12 @@ class seq_atom extends uvm_sequence #(decoder_transaction_in);
 	    tx.restrict_32_regreg.constraint_mode(0);
 	    tx.restrict_fp_32_regreg_legal.constraint_mode(0);
 	    tx.restrict_control_legal.constraint_mode(0);
+	    tx.restrict_integer_reg.constraint_mode(0);
+            tx.restrict_reg_imm_funct3.constraint_mode(0);
+            tx.restrict_reg_imm_funct7_sll.constraint_mode(0);
+            tx.restrict_reg_imm_funt7_srl_sra.constraint_mode(0);
+            tx.restrict_vec_float.constraint_mode(0);
+
             start_item(tx);
             assert(tx.randomize());
             finish_item(tx);
@@ -150,7 +169,13 @@ class seq_32_regreg extends uvm_sequence #(decoder_transaction_in);
 	    tx.restrict_32_regreg.constraint_mode(1);
 	    tx.restrict_fp_32_regreg_legal.constraint_mode(0);
 	    tx.restrict_control_legal.constraint_mode(0);
-            start_item(tx);
+            tx.restrict_integer_reg.constraint_mode(0);
+            tx.restrict_reg_imm_funct3.constraint_mode(0);
+            tx.restrict_reg_imm_funct7_sll.constraint_mode(0);
+            tx.restrict_reg_imm_funt7_srl_sra.constraint_mode(0);
+            tx.restrict_vec_float.constraint_mode(0);
+
+	    start_item(tx);
             assert(tx.randomize());
             finish_item(tx);
         endtask: body
@@ -174,6 +199,12 @@ class seq_fp_32_regreg extends uvm_sequence #(decoder_transaction_in);
 	    tx.restrict_32_regreg.constraint_mode(0);
 	    tx.restrict_fp_32_regreg_legal.constraint_mode(1);
 	    tx.restrict_control_legal.constraint_mode(0);
+	    tx.restrict_integer_reg.constraint_mode(0);
+            tx.restrict_reg_imm_funct3.constraint_mode(0);
+            tx.restrict_reg_imm_funct7_sll.constraint_mode(0);
+            tx.restrict_reg_imm_funt7_srl_sra.constraint_mode(0);
+            tx.restrict_vec_float.constraint_mode(0);
+
             start_item(tx);
             assert(tx.randomize());
             finish_item(tx);
@@ -198,11 +229,108 @@ class seq_control extends uvm_sequence #(decoder_transaction_in);
 	    tx.restrict_32_regreg.constraint_mode(0);
 	    tx.restrict_fp_32_regreg_legal.constraint_mode(0);
 	    tx.restrict_control_legal.constraint_mode(1);
+	    tx.restrict_integer_reg.constraint_mode(0);
+            tx.restrict_reg_imm_funct3.constraint_mode(0);
+            tx.restrict_reg_imm_funct7_sll.constraint_mode(0);
+            tx.restrict_reg_imm_funt7_srl_sra.constraint_mode(0);
+            tx.restrict_vec_float.constraint_mode(0);
+
             start_item(tx);
             assert(tx.randomize());
             finish_item(tx);
         endtask: body
     endclass: seq_control
+
+class seq_int_reg extends uvm_sequence #(decoder_transaction_in);
+        `uvm_object_utils(seq_control)
+
+        function new(string name = "");
+            super.new(name);
+        endfunction: new
+
+        task body;
+            decoder_transaction_in tx;
+            tx=decoder_transaction_in::type_id::create("tx");
+            tx.restrict_ex_valid.constraint_mode(1);
+            tx.restrict_load_store.constraint_mode(0);
+            tx.restrict_store_legal.constraint_mode(0);
+            tx.restrict_load_legal.constraint_mode(0);
+            tx.restrict_atomic_legal.constraint_mode(0);
+            tx.restrict_32_regreg.constraint_mode(0);
+            tx.restrict_fp_32_regreg_legal.constraint_mode(0);
+            tx.restrict_control_legal.constraint_mode(0);
+	    tx.restrict_integer_reg.constraint_mode(1);
+            tx.restrict_reg_imm_funct3.constraint_mode(0);
+            tx.restrict_reg_imm_funct7_sll.constraint_mode(0);
+            tx.restrict_reg_imm_funt7_srl_sra.constraint_mode(0);
+            tx.restrict_vec_float.constraint_mode(0);
+
+            start_item(tx);
+            assert(tx.randomize());
+            finish_item(tx);
+        endtask: body
+    endclass: seq_int_reg
+
+class seq_reg_imm extends uvm_sequence #(decoder_transaction_in);
+        `uvm_object_utils(seq_control)
+
+        function new(string name = "");
+            super.new(name);
+        endfunction: new
+
+        task body;
+            decoder_transaction_in tx;
+            tx=decoder_transaction_in::type_id::create("tx");
+            tx.restrict_ex_valid.constraint_mode(1);
+            tx.restrict_load_store.constraint_mode(0);
+            tx.restrict_store_legal.constraint_mode(0);
+            tx.restrict_load_legal.constraint_mode(0);
+            tx.restrict_atomic_legal.constraint_mode(0);
+            tx.restrict_32_regreg.constraint_mode(0);
+            tx.restrict_fp_32_regreg_legal.constraint_mode(0);
+            tx.restrict_control_legal.constraint_mode(0);
+            tx.restrict_integer_reg.constraint_mode(0);
+            tx.restrict_reg_imm_funct3.constraint_mode(1);
+            tx.restrict_reg_imm_funct7_sll.constraint_mode(1);
+            tx.restrict_reg_imm_funt7_srl_sra.constraint_mode(1);
+            tx.restrict_vec_float.constraint_mode(0);
+
+	    start_item(tx);
+            assert(tx.randomize());
+            finish_item(tx);
+        endtask: body
+    endclass: seq_reg_imm
+
+class seq_vec_float extends uvm_sequence #(decoder_transaction_in);
+        `uvm_object_utils(seq_control)
+
+        function new(string name = "");
+            super.new(name);
+        endfunction: new
+
+        task body;
+            decoder_transaction_in tx;
+            tx=decoder_transaction_in::type_id::create("tx");
+            tx.restrict_ex_valid.constraint_mode(1);
+            tx.restrict_load_store.constraint_mode(0);
+            tx.restrict_store_legal.constraint_mode(0);
+            tx.restrict_load_legal.constraint_mode(0);
+            tx.restrict_atomic_legal.constraint_mode(0);
+            tx.restrict_32_regreg.constraint_mode(0);
+            tx.restrict_fp_32_regreg_legal.constraint_mode(0);
+            tx.restrict_control_legal.constraint_mode(0);
+            tx.restrict_integer_reg.constraint_mode(0);
+            tx.restrict_reg_imm_funct3.constraint_mode(0);
+            tx.restrict_reg_imm_funct7_sll.constraint_mode(0);
+            tx.restrict_reg_imm_funt7_srl_sra.constraint_mode(0);
+            tx.restrict_vec_float.constraint_mode(1);
+
+	    start_item(tx);
+            assert(tx.randomize());
+            finish_item(tx);
+        endtask: body
+    endclass: seq_vec_float
+
 
 
     class seq_of_commands extends uvm_sequence #(decoder_transaction_in);
@@ -221,13 +349,18 @@ class seq_control extends uvm_sequence #(decoder_transaction_in);
            	seq_32_regreg seq3;
            	seq_fp_32_regreg seq4;
            	seq_control seq5;
-                
+		seq_int_reg seq6;
+		seq_reg_imm seq7;
+		seq_vec_float seq8;                
+
 		seq = simple_seq::type_id::create("seq");
                 seq2 = seq_atom::type_id::create("seq2");
                 seq3 = seq_32_regreg::type_id::create("seq3");
                 seq4 = seq_fp_32_regreg::type_id::create("seq4");
                 seq5 = seq_control::type_id::create("seq5");
-
+		seq6 = seq_int_reg::type_id::create("seq6");
+		seq7 = seq_reg_imm::type_id::create("seq7");
+		seq8 = seq_vec_float::type_id::create("seq8");
 
                 assert( seq.randomize() );
                 seq.start(p_sequencer);
@@ -243,6 +376,15 @@ class seq_control extends uvm_sequence #(decoder_transaction_in);
 
 	        assert( seq5.randomize() );
                 seq5.start(p_sequencer);
+
+		assert( seq6.randomize() );
+                seq6.start(p_sequencer);
+
+		assert( seq7.randomize() );
+                seq7.start(p_sequencer);
+
+		assert( seq8.randomize() );
+                seq8.start(p_sequencer);
 
             end
         endtask: body
