8982337dfee99372ab371198ff15ff2176fae1ba
jismisk <jismisabuk@utexas.edu>
2019-05-07 12:13:02 -0500

    more assertions

--- ariane/formal_tb/bind_wrapper.sv
+++ ariane/formal_tb/bind_wrapper.sv
@@ -10,11 +10,31 @@ module alu_verif(
 );
 
 
+
+logic equal;
+assign equal = ((fu_data_i.operand_a) == (fu_data_i.operand_b));
+
+
 // Write properties here
+
+
+//ASSUMES
+
+
+
+
+
+
+
+//ASSERTS
 
 assert_prop_check_eq : assert property (@(posedge clk_i) disable iff(~rst_ni) (fu_data_i.operator == EQ) && ((fu_data_i.operand_a) == (fu_data_i.operand_b)) |-> alu_branch_res_o);  
 
 
+
+//Compares
+assume_prop_check_eq : assert property (@(posedge clk_i) disable iff(~rst_ni) (fu_data_i.operator == EQ) |-> (result_o == {{63{0}},equal}));  
+assume_prop_check_neq : assert property (@(posedge clk_i) disable iff(~rst_ni) (fu_data_i.operator == NE) |-> (result_o == {{63{0}},~equal}));  
 
 // Logic operation
 assert_prop_check_and : assert property (@(posedge clk_i) disable iff(~rst_ni) (fu_data_i.operator == ANDL) |-> (result_o == (fu_data_i.operand_a & fu_data_i.operand_b)));  
@@ -25,7 +45,7 @@ assert_prop_check_xor : assert property (@(posedge clk_i) disable iff(~rst_ni) (
 // Arithmetic
 assert_prop_check_add : assert property (@(posedge clk_i) disable iff(~rst_ni) (fu_data_i.operator == ADD) |-> (result_o == (fu_data_i.operand_a + fu_data_i.operand_b)));
 
-assert_prop_check_sub : assert property (@(posedge clk_i) disable iff(~rst_ni) (fu_data_i.operator == SUB) |-> (result_o == (fu_data_i.operand_a - fu_data_i.operand_b)));
+assert_prop_check_sub : assert property (@(posedge clk_i) disable iff(~rst_ni) (fu_data_i.operator == SUB) |-> (result_o == (fu_data_i.operand_a +(fu_data_i.operand_b ^ {64{1}} + 1))));
 
 
 endmodule
